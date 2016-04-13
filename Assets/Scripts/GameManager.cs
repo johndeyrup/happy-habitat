51bdb071ff455403d89648568355a5dcbc3adb4d
print hi <john.deyrup@gmail.com>
2016-04-13 10:27:28 -0400

    Added base assets and scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+    private int level = 1;
+    private Camera cam;
+    private float scrollSpeed;
+    private int scrollBound;
+
+    void Start()
+    {
+        cam = Camera.main;
+        scrollSpeed = 10;
+        scrollBound = 5;
+    }
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    void FixedUpdate()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        if (mouseX < scrollBound)
+        {
+            cam.transform.Translate(Vector3.right * -scrollSpeed);
+        }
+        else if (mouseY < scrollBound)
+        {
+            cam.transform.Translate(Vector3.forward * -scrollSpeed);
+        }
+        else if (mouseX >= Screen.height - scrollBound)
+        {
+            cam.transform.Translate(Vector3.right);
+        }
+        else if (mouseY >= Screen.height - scrollBound) {
+            cam.transform.Translate(Vector3.forward);
+        }
+        cam.transform.position = new Vector3(Mathf.Clamp(cam.transform.position.x, 0, 10), Mathf.Clamp(cam.transform.position.y, 0, 10), 1.0f);
+    }
+}
+
